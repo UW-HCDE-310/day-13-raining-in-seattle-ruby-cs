dedad364fdb7f633a0f4a2bb536c7290129b1427
Ruby <18ruby.cs@gmail.com>
2024-11-12 19:19:56 -0800

    is it raining in seattle

---
+++ seattle_rain.py
@@ -0,0 +1,13 @@
+import urllib
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+
+def is_it_raining_in_seattle():
+    with urllib.request.urlopen('https://depts.washington.edu/ledlab/teaching/is-it-raining-in-seattle/') as response:
+        is_it_raining_in_seattle = response.read().decode()
+
+    if is_it_raining_in_seattle == "true":
+        return True
+    else:
+        return False
